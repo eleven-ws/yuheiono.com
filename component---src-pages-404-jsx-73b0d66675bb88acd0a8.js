5d53fa69f2d8e991eea950f2317204e65e434f07
Kojiro Ozaki <koujirou0111@gmail.com>
2023-12-30 01:21:28 +0900

    Updates

--- component---src-pages-404-jsx-73b0d66675bb88acd0a8.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkyuheiono_com=self.webpackChunkyuheiono_com||[]).push([[256],{8336:function(t,e,n){n.r(e),n.d(e,{Head:function(){return u}});var a=n(2784),o=n(8165);e.default=()=>(0,o.tZ)(a.Fragment,null);const u=t=>{const{data:{site:{siteMetadata:{siteUrl:e}}}}=t;return(0,o.tZ)(a.Fragment,null,(0,o.tZ)("meta",{name:"robots",content:"noindex"}),(0,o.tZ)("meta",{httpEquiv:"Refresh",content:`0; URL=${e}`}))}}}]);
-//# sourceMappingURL=component---src-pages-404-jsx-73b0d66675bb88acd0a8.js.map
